d26e41967d8a180f8cd55dbaac98535ef28ca0e7
DaMbY1 <dmitrybab80@mail.ru>
2026-03-08 21:27:03 +0300

    Update Problem2.sql

--- Problem2.sql
+++ Problem2.sql
@@ -1,1 +1,19 @@
+WITH avg_scores AS (SELECT last_name, subject, ROUND(AVG(score)) as aver
+FROM Journal
+GROUP BY last_name, subject),
 
+C AS (SELECT DISTINCT last_name
+FROM avg_scores
+WHERE aver < 4),
+
+D AS (SELECT DISTINCT last_name
+FROM Journal
+WHERE subject = "Математика" and score = 2),
+
+A AS (SELECT last_name
+FROM avg_scores
+WHERE subject = "Математика" and aver = 5)
+
+SELECT DISTINCT last_name
+FROM Journal 
+WHERE last_name IN A AND last_name NOT IN D AND last_name NOT IN C;
