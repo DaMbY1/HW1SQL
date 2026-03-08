340c1605f731c4e5dbe5ddc3abf9ed04e07f3e00
DaMbY1 <dmitrybab80@mail.ru>
2026-03-08 21:50:09 +0300

    Update Problem3.sql

--- Problem3.sql
+++ Problem3.sql
@@ -1,1 +1,6 @@
-
+SELECT user_id
+FROM watch_content
+GROUP BY user_id
+HAVING COUNT(CASE WHEN video_id = 1 THEN 1 END) > 0   -- смотрел видео 1
+   AND COUNT(CASE WHEN video_id = 3 THEN 1 END) > 0   -- смотрел видео 3
+   AND COUNT(CASE WHEN video_id = 2 THEN 1 END) = 0;  -- не смотрел видео 2
