79c2983953eb8e067604fd607be95aa4f74c08ea
DaMbY1 <dmitrybab80@mail.ru>
2026-03-08 21:19:43 +0300

    Update Problem1.sql

--- Problem1.sql
+++ Problem1.sql
@@ -1,1 +1,5 @@
-
+SELECT name
+FROM employer AS emp
+LEFT JOIN vacancy AS vac ON emp.employer_id = vac.employer_id AND vac.active = TRUE
+GROUP BY emp.employer_id, emp.name
+HAVING COUNT(active) <= 5;
