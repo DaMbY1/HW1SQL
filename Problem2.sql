c968899d4e1c08377271b65b44b14a42ebbb932e
DaMbY1 <dmitrybab80@mail.ru>
2026-03-08 21:41:52 +0300

    Update Problem2.sql

--- Problem2.sql
+++ Problem2.sql
@@ -1,19 +1,25 @@
-WITH avg_scores AS (SELECT last_name, subject, ROUND(AVG(score)) as aver
-FROM Journal
-GROUP BY last_name, subject),
-
-C AS (SELECT DISTINCT last_name
-FROM avg_scores
-WHERE aver < 4),
-
-D AS (SELECT DISTINCT last_name
-FROM Journal
-WHERE subject = "Математика" and score = 2),
-
-A AS (SELECT last_name
-FROM avg_scores
-WHERE subject = "Математика" and aver = 5)
-
+WITH avg_scores AS (
+    SELECT last_name, subject, ROUND(AVG(score)) AS aver
+    FROM Journal
+    GROUP BY last_name, subject
+),
+C AS (
+    SELECT DISTINCT last_name
+    FROM avg_scores
+    WHERE aver < 4
+),
+D AS (
+    SELECT DISTINCT last_name
+    FROM Journal
+    WHERE subject = 'Математика' AND score = 2
+),
+A AS (
+    SELECT last_name
+    FROM avg_scores
+    WHERE subject = 'Математика' AND aver = 5
+)
 SELECT DISTINCT last_name
 FROM Journal 
-WHERE last_name IN A AND last_name NOT IN D AND last_name NOT IN C;
+WHERE last_name IN (SELECT last_name FROM A)
+  AND last_name NOT IN (SELECT last_name FROM D)
+  AND last_name NOT IN (SELECT last_name FROM C);
